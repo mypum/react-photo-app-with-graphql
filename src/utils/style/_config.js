5c668a49ae2d9f4b67a6f226f4a01abcfb9320d4
Thongchai Kitiyanantawong <pum@Pums-MacBook-Pro.local>
2017-10-01 18:45:55 +0700

    Add utils for style

---
+++ src/utils/style/_config.js
@@ -0,0 +1,3 @@
+export const color = {
+  primary: '#c9ad66'
+}
---
+++ src/utils/style/index.js
@@ -0,0 +1,12 @@
+export function mainFont (weight = false, style = false) {
+  let value = [`font-family: 'Helvetica Neue, Helvetica, Arial, sans-serif';`]
+
+  if (weight) {
+    value.push(`font-weight: ${weight};`)
+  }
+  if (style) {
+    value.push(`font-style: ${style};`)
+  }
+
+  return value.join(' ')
+}
