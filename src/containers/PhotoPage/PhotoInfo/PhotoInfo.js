8318c06ab1c09ccb457e500e7f79bc3600e349a8
Thongchai Kitiyanantawong <pum@Pums-MacBook-Pro.local>
2017-10-04 03:45:15 +0700

    Add icon for camera info

--- src/containers/PhotoPage/PhotoInfo/PhotoInfo.js
+++ src/containers/PhotoPage/PhotoInfo/PhotoInfo.js
@@ -15,14 +15,40 @@ function PhotoInfo (props) {
 
   return (
     <div className="photoInfo">
-      { !!camera && <h2>{camera}</h2> }
-      { !!lens && <h3>{lens}</h3> }
+      <div className="cameraInfo">
+        { !!camera && <h2>{camera}</h2> }
+        { !!lens && <h3>Lens: {lens}</h3> }
+      </div>
       <ul>
-        { !!aperture && <li>F{aperture}</li> }
-        { !!focalLength && <li>{focalLength}mm</li> }
-        { !!shutterSpeed && <li>{shutterSpeed}s</li> }
-        { !!iso && <li>iso {iso}</li> }
+        { !!aperture && <li><img className="icon" src="/static/images/aperture-icon.png" /><span>ƒ/{aperture}</span></li> }
+        { !!focalLength && <li><img className="icon" src="/static/images/focuslength-icon.png" /><span>{focalLength}mm</span></li>}
+        { !!shutterSpeed && <li><img className="icon" src="/static/images/speedshutter-icon.png" /><span>{shutterSpeed}s</span></li> }
+        { !!iso && <li><img className="icon" src="/static/images/iso-icon.png" /><span>{iso}</span></li> }
       </ul>
+      <style jsx>{`
+        .cameraInfo {
+          margin-bottom: 15px;
+        }
+        h2 {
+          font-size: 18px;
+          margin-bottom: 5px;
+        }
+        .icon {
+          width: 25px;
+          height: auto;
+          margin-right: 15px;
+        }
+        li {
+          height: 30px;
+          display: flex;
+          align-items: center;
+          align-content: flex-start;
+          margin-bottom: 10px;
+        }
+        li span {
+          font-size: 16px;
+        }
+      `}</style>
     </div>
   )
 }
